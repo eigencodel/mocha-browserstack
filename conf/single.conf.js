34b9811b9e355f11f6d5d6540b6f202755889df1
eigencodel <marcelbutucea@gmail.com>
2017-10-19 14:38:53 +0800

    Update single.conf.js

--- conf/single.conf.js
+++ conf/single.conf.js
@@ -1,26 +1,9 @@
-var config = {
-  'commanCapabilities': {
-    'browserstack.user': process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
-    'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
-    'build': 'mocha-browserstack',
-    'name': 'single_test',
-    'browserstack.debug': 'true',
-  },
-  'multiCapabilities': [{
-      'browserName': 'Chrome'
-    },{
-      'browserName': 'Safari'
-    },{
-      'browserName': 'Firefox'
-    },{
-      'browserName': 'IE'
-  }]
-};
+exports.capabilities = {
+  'browserstack.user': process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
+  'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
+  'build': 'mocha-browserstack',
+  'name': 'single_test',
+  'browserstack.debug': 'true',
 
-exports.capabilities = [];
-// Code to support common capabilities
-config.multiCapabilities.forEach(function(caps) {
-  var temp_caps = JSON.parse(JSON.stringify(config.commanCapabilities));
-  for(var i in caps) temp_caps[i] = caps[i];
-  exports.capabilities.push(temp_caps);
-});
+  'browserName': 'chrome'
+};
